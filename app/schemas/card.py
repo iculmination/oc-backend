1e972e471fb99041b7fe2cd2cca6ea8e3674b7a0
iculmination <ovsiichuk.bohdan@gmail.com>
2026-03-17 19:42:29 +0100

    feat: cards crud

--- app/schemas/card.py
+++ app/schemas/card.py
@@ -1,12 +1,17 @@
+from datetime import datetime
 from pydantic import BaseModel
 from uuid import UUID
 from app.enums.card import CardAbilities, CardNature
 
+
 class CardSchema(BaseModel):
-    id: UUID
     name: str
     attack: int
-    health: int
     max_health: int
     ability: CardAbilities
-    nature: CardNature 
+    nature: CardNature
+
+class CardReadSchema(CardSchema):
+    id: UUID
+    created_at: datetime
+    updated_at: datetime
